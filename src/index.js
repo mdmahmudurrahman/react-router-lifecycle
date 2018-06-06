0636669c47c55ebb8c92f2a446ad0b9d7d6d219c
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-06 11:39:37 +0700

    Install react-router-dom and add routing for some component

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/profile" component={Profile} />
+      </div>  
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
