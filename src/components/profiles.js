03b505a32d6792d8eb22c001a97bdf881243e601
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-06 15:09:11 +0700

    Redirect to root, redirecting to 404 component

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 const Profile = (props) => {
   console.log(props);
+
+  const redirect = () => {
+    // return <Redirect to="/" />
+    // or
+    return props.history.push('/')
+  } 
   return(
     <div>
       <Link to={{
         pathname: `${props.match.url}/posts`
       }}>Take me to /profile/posts</Link>
+
+      {redirect()}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ const App = () => {
           <Route path="/posts/:id/:username" component={PostItem} />
           <Route path="/posts" component={Posts} />
           <Route path="/profile" component={Profile} />
-          <Route path="/" component={Home} />
+          <Route path="/" exact component={Home} />
+          <Route render={() => <h3>Opps!! 404</h3>} />
         </Switch>  
       </div>  
     </BrowserRouter>
