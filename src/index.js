81e8cf04fb8c586a0d77f01ed1be395297160147
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-06 11:49:45 +0700

    Add the static link to the component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Home from './components/home';
 import Posts from './components/posts';
@@ -10,6 +10,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <div>
+        <header>
+          <Link to="/">Home</Link><br/>
+          <Link to="/posts">Posts</Link><br/>
+          <Link to="/profile">Profile</Link><br/>
+          <hr/>
+        </header>
         <Route path="/" exact component={Home} />
         <Route path="/posts" component={Posts} />
         <Route path="/profile" component={Profile} />
