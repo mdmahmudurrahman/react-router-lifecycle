327e575d53b0e8a6577d67c292fcd7e2a6b0aea4
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-07 16:08:30 +0700

    returning arrays

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Posts = (props) => {
+  const ids = [
+    {id: '1', name: 'Post 1'}, 
+    {id: '2', name: 'Post 2'}, 
+    {id: '3', name: 'Post 3'}, 
+  ]
+
+  const list = ids.map(item => {
+    return(
+      <span key={item.id}>
+        <Link to={{
+          pathname: `${props.match.url}/${item.id}/${item.name}`
+          }}>{item.name}</Link><br/>
+      </span>
+    )
+  })
+
   return(
     <div>
       <p><Link to={{
@@ -13,6 +29,12 @@ const Posts = (props) => {
       <p><Link to={{
         pathname: `${props.match.url}/3/lest`
       }}>Post 3</Link></p>
+
+      <div>
+        {
+          list
+        }
+      </div>
     </div>
   )
 }
